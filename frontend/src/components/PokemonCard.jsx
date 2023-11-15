31f4791271f6849fcba84c25206f7a18b0b12354
Ribeiro Jessica <ribeirofj@yahoo.com>
2023-11-15 17:04:10 +0100

    ajout mew

--- frontend/src/components/PokemonCard.jsx
+++ frontend/src/components/PokemonCard.jsx
@@ -1,13 +1,27 @@
+const pokemonList = [
+  {
+    name: "bulbasaur",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+  },
+  {
+    name: "mew",
+  },
+];
+
 export default function PokemonCard() {
+  const pokemon = pokemonList[0];
+
   return (
     <div>
       <figure>
-        <img
-          src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"
-          alt="Bulbizard"
-        />
-        <figcaption>Bulbizard</figcaption>
+        {pokemon.imgSrc ? (
+          <img src={pokemon.imgSrc} alt={pokemon.name} />
+        ) : (
+          "???"
+        )}
       </figure>
+      <figcaption>{pokemon.name}</figcaption>
     </div>
   );
 }
